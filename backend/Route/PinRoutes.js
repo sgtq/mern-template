7588d4a0fbbba85693cd3aa9410e1488a683ffc7
Gaston <gaston.quittner@gmail.com>
2023-01-26 17:09:01 -0600

    Template commit

--- backend/Route/PinRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createPin, getAllPins } from "../Controller/PinController.js";
-
-const router = express.Router();
-
-router.get("/", getAllPins);
-router.post("/", createPin);
-
-export default router;
